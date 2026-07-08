1bfa68e11ddb3ed5b23fafe54b9d60854cac3721
AshishPatil05 <ashishpatil05050@gmail.com>
2026-07-08 12:47:14 +0530

    CReated COntact MOdel

--- home/views.py
+++ home/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render,HttpResponse
+from home.models import Contact
 
 # Create your views here.
 def home(request):
@@ -8,6 +9,13 @@ def aboutus(request):
     return render(request,'aboutus.html')
 
 def contact(request):
+
+    if request.method == "POST":
+        name = request.POST.get('name')
+        email = request.POST.get('email')
+        message = request.POST.get('message')
+        contact = Contact(name=name,email=email,message=message)
+        contact.save()
     return render(request,'contact.html')
 
 def course(request):
